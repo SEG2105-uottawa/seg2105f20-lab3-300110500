b2ae78c8481081a4d773e43910e6280fbfe8f38e
Coding0116 <sianabinkong@gmail.com>
2020-09-29 16:07:06 -0400

    getting rid of the commented out codes

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -75,47 +75,6 @@ public class MainActivity extends AppCompatActivity {
         data1 = Double.parseDouble(eText.getText().toString());
         eText.setText("");
     }
-/*
-    public void btn00Click(View view){
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"0");
-    }
-    public void btn01Click(View view){
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"1");
-    }
-    public void btn02Click(View view){
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"2");
-    }
-    public void btn03Click(View view){
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"3");
-    }
-    public void btn04Click(View view){
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"4");
-    }
-    public void btn05Click(View view) {
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"5");
-    }
-    public void btn06Click(View view){
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"6");
-    }
-    public void btn07Click(View view){
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"7");
-    }
-    public void btn08Click(View view){
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"8");
-    }
-    public void btn09Click(View view){
-        EditText eText = (EditText)findViewById(R.id.resultEdit);
-        eText.setText(eText.getText()+"9");
-    }*/
 
     public void onClickNumericalButton(View view){
         int pressID = view.getId();
